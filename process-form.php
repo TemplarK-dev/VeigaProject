5143eac232b60f4ca60e6c320a2a8cbf8bbd040b
TemplarK-dev <59141074+TemplarK-dev@users.noreply.github.com>
2023-07-09 15:57:03 +0200

    Update process-form.php

--- process-form.php
+++ process-form.php
@@ -1,4 +1,10 @@
 <?php
 
-print_r($_POST);
+$name = $_POST["name"];
+$id = $_POST["id"];
+$message = $_POST["message"];
+$severity = filter_input(INPUT_POST, "severity", FILTER_VALIDATE_INT);
+$type = filter_input(INPUT_POST, "type", FILTER_VALIDATE_INT);
+
+var_dump($name, $id, $message, $severity, $type);
 
